a9a150aa8269e3072919ae8e757aad894add1b81
Lorival Cardoso <looricardoso@hotmail.com>
2020-11-22 20:25:24 -0300

    Correção da página inicial

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,8 @@ namespace QuemSouEuAPI
 
             // app.UseHttpsRedirection();
 
+            app.UseDefaultFiles();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
